2a2ed2863de5d03876f0ef496788397f0958fcb6
Muharem Hrnjadovic <muharem@linux.com>
2019-07-15 14:45:58 +0200

    implement sample template

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,11 @@ fn main() {
     let matches = app.get_matches();
 
     if let Some(matches) = matches.subcommand_matches("sample") {
-        println!("Generate sample files");
         if let Some(_matches) = matches.subcommand_matches("config") {
             println!("{}", gen_sample_config(crate_name!(), crate_version!()));
         }
         if let Some(_matches) = matches.subcommand_matches("template") {
-            println!("!! template sample");
+            println!("{}", gen_sample_template(crate_name!(), crate_version!()));
         }
     } else if let Some(matches) = matches.subcommand_matches("run") {
         println!("Run mailer tool");
@@ -49,6 +48,20 @@ mm@gmail.com=Mickey Mouse|ORG:-Disney   # trailing comment!!
 # The 'Cc' setting below *adds* to the global 'Cc' value above
 daisy@example.com=Daisy Lila|ORG:-NASA|TITLE:-Dr.|Cc:-+inc@gg.org"#,
         name, version
+    );
+    sample
+}
+
+fn gen_sample_template(name: &str, version: &str) -> String {
+    let sample = format!(
+        r#"FN / LN / EA = first name / last name / email address
+
+Hello %FN% // %LN%, how are things going at %ORG%?
+this is your email: %EA% :)
+
+
+Sent with {} version {}, see https://301.mx/{} for details"#,
+        name, version, name
     );
     sample
 }
