1a4fc94402a26cafb023bb3c10573ded56355e44
Muharem Hrnjadovic <muharem@linux.com>
2019-07-16 06:40:06 +0200

    check config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use ini::Ini;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
@@ -21,11 +22,44 @@ fn main() {
     } else if let Some(matches) = matches.subcommand_matches("run") {
         println!("Run mailer tool");
         if matches.is_present("dry_run") {
-            println!("!! dry run, no action");
+            println!("* dry run, no action");
         } else {
-            println!("!! run the mailer");
+            println!("* run the mailer");
+        }
+        let config = matches.value_of("config").unwrap();
+        let i = Ini::load_from_file(config).unwrap();
+        match check_config(&i) {
+            Ok(_) => println!("* config looks good"),
+            Err(msg) => println!("!! invalid config -- {:?}", msg),
         }
     }
+}
+
+fn check_config(cfg: &ini::Ini) -> Result<usize, String> {
+    let sections = ["general", "recipients"];
+    let mut num_recipients = 0;
+
+    for s in sections.iter() {
+        match cfg.section(Some(s.to_string())) {
+            Some(props) => {
+                if *s == "general" {
+                    if !props.contains_key("From") {
+                        return Err(String::from("No from header in the general section"));
+                    }
+                }
+                if *s == "recipients" {
+                    num_recipients = props.len();
+                    if num_recipients == 0 {
+                        return Err(String::from("No email recipients found in config file"));
+                    }
+                }
+            }
+            None => {
+                return Err(format!("No *{}* section in config file", s));
+            }
+        }
+    }
+    Ok(num_recipients)
 }
 
 fn gen_sample_config(name: &str, version: &str) -> String {
@@ -62,4 +96,92 @@ this is your email: %EA% :)
 Sent with {} version {}, see https://301.mx/{} for details"#,
         name, version, name
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ini::Ini;
+    use std::io::{Error, Write};
+    use tempfile::NamedTempFile;
+
+    fn prep_config(content: &str) -> Result<ini::Ini, Error> {
+        let mut tf = NamedTempFile::new()?;
+
+        // Write some test data to the first handle.
+        tf.write_all(content.as_bytes())?;
+        let cfg = Ini::load_from_file(tf.path()).unwrap();
+        Ok(cfg)
+    }
+    #[test]
+    fn check_config_with_empty_file() {
+        let cfg = prep_config("").expect("Failed to set up config");;
+        assert_eq!(
+            Err(String::from("No *general* section in config file")),
+            check_config(&cfg)
+        );
+    }
+    #[test]
+    fn check_config_with_no_recipients_section() {
+        let file = r#"
+[general]
+From=abc@def.com
+# this is a comment"#;
+        let cfg = prep_config(file).expect("Failed to set up config");;
+        assert_eq!(
+            Err(String::from("No *recipients* section in config file")),
+            check_config(&cfg)
+        );
+    }
+    #[test]
+    fn check_config_with_no_recipients() {
+        let file = r#"
+[general]
+From=abc@def.com
+# this is a comment
+[recipients]"#;
+        let cfg = prep_config(file).expect("Failed to set up config");;
+        assert_eq!(
+            Err(String::from("No email recipients found in config file")),
+            check_config(&cfg)
+        );
+    }
+    #[test]
+    fn check_config_with_no_from_in_empty_general() {
+        let file = r#"
+[general]
+# this is a comment
+[recipients]"#;
+        let cfg = prep_config(file).expect("Failed to set up config");;
+        assert_eq!(
+            Err(String::from("No from header in the general section")),
+            check_config(&cfg)
+        );
+    }
+    #[test]
+    fn check_config_with_no_from_in_non_empty_general() {
+        let file = r#"
+[general]
+P1=a
+P2=b
+# this is a comment
+[recipients]"#;
+        let cfg = prep_config(file).expect("Failed to set up config");;
+        assert_eq!(
+            Err(String::from("No from header in the general section")),
+            check_config(&cfg)
+        );
+    }
+    #[test]
+    fn check_config_with_happy_path() {
+        let file = r#"
+[general]
+From=abc@def.com
+# this is a comment
+[recipients]
+a@b.com=A B
+c@d.com=C D"#;
+        let cfg = prep_config(file).expect("Failed to set up config");;
+        assert_eq!(Ok(2), check_config(&cfg));
+    }
 }
